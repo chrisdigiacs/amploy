58ff0ab1b681be6f16e5f5289001b96949244f23
chrisdigiacs <chrisdigiacomo13@gmail.com>
2021-02-21 02:49:47 -0500

    Created employee account page linked to home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './component/Login';
 import Home from './component/Home';
 import SignUp from './component/SignUp';
+import Account from './component/EmployeeAccount';
+import './App.css';
 
 function App() {
  
@@ -12,8 +14,8 @@ function App() {
           <Route path = '/' exact component={Login}/>
           <Route path = '/Home' exact component={Home}/>
           <Route path = '/SignUp' exact component={SignUp}/>
+          <Route path = '/Home/Account' exact component ={Account}/>
 
-        
       </div>
     </Router>
   );
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 function Home() {
     return (
         <div className ="Home">
             <h1>Home page</h1>
+            <Link to = '/Home/Account'> <input type="submit" value="Account"/> </Link>
         </div>
     )
 }
